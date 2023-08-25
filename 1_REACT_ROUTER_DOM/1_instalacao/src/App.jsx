b967643600b14e57c7293192d04043f0be9a3b30
Bruno Sutil <brunosutil30@gmail.com>
2023-08-25 14:48:22 -0300

    primeira rota

---
+++ 1_REACT_ROUTER_DOM/1_instalacao/src/App.jsx
@@ -0,0 +1,10 @@
+  /*baixando a estenção es7+ React...   e digitando no APP (rfce) vc criará uma conexão*/
+  /*para baixar o react routwer dom, precisamos ir no terminal e colocar npm i react-router-dom */
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
