01fd302592aec92b50a61e7fa10c66c60fd10b6a
Bruno Sutil <brunosutil30@gmail.com>
2023-09-22 15:47:43 -0300

    projeto finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -2,7 +2,13 @@ import Header from "./components/Header"
 
 function App() {
   return (
+    <div>
     <Header/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quia recusandae iste quidem pariatur laborum veniam culpa. Laudantium fuga doloremque neque atque provident. Perferendis voluptate sed assumenda repellendus, quisquam beatae?
+    Architecto eius nisi omnis magnam deserunt dolorum harum impedit nulla mollitia adipisci obcaecati accusamus consectetur deleniti, est, praesentium itaque quod odio vel voluptas fugit! Officiis esse ipsa dolorum amet ipsum.
+    Blanditiis consectetur vitae, obcaecati ipsa ad, at nemo asperiores provident quos impedit odio tempora beatae minima! Sapiente ut nemo nostrum sequi magni cumque accusantium. Vel numquam illum voluptate consequuntur harum.
+    Vel, animi cum. Sunt enim culpa, laboriosam porro, rem ipsa labore quibusdam consequatur aperiam id accusantium veniam assumenda earum mollitia odio ullam quia laborum, animi et obcaecati. Ullam, alias architecto!</p>
+    </div>
   )
 }
 
